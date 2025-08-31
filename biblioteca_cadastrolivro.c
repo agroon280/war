a386364e54c02c5eb419ba19a3c9afb300efb3cc
agroon280 <caurrodrigo@gmail.com>
2025-08-31 21:13:24 +0000

    atualizaçao

--- biblioteca_cadastrolivro.c
+++ biblioteca_cadastrolivro.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+typedef struct{
+ char nome[20];
+ char autor[20];
+ char editora[20];
+ int ediçao;
+}cadastro;
+
 int main(){
-
+ cadastro c1 = {"Asa branca", "flavia fontinele", "casabranca", 2018};
+ printf("nome do livro: %s \n",c1.nome);
+ printf("autor(a): %s \n", c1.autor);
     
-
+return 0;
 }
