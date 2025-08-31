a88a399c184d5c7910aac2b4284f1d636c385ebe
agroon280 <caurrodrigo@gmail.com>
2025-08-31 21:24:38 +0000

    a

--- biblioteca_cadastrolivro.c
+++ biblioteca_cadastrolivro.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+//definindo a struct e colocando os dados
+//usando typedef para codigo mais limpo
 
 typedef struct{
  char nome[20];
  char autor[20];
  char editora[20];
  int ediçao;
-}cadastro;
+}cadastro;//cadastro passa a ser o nome que puxa toda a estrutura 
+//com uso do typedef
 
 int main(){
+//dando valor as variaveis dentro da estrutura de acordo com as variaveis.
  cadastro c1 = {"Asa branca", "flavia fontinele", "casabranca", 2018};
- printf("nome do livro: %s \n",c1.nome);
- printf("autor(a): %s \n", c1.autor);
+  printf("nome do livro: %s \n",c1.nome);
+  printf("Autor(a): %s \n", c1.autor);
     
 return 0;
 }
