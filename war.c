e98ec64e1936b3b60d912c9da7ed499bcdd8d14a
agroon280 <caurrodrigo@gmail.com>
2025-09-01 15:47:38 +0000

    completo

--- war.c
+++ war.c
@@ -7,53 +7,88 @@
 
 //decidindo os arrays
 struct territ{
-char pais[tamanho_max];
-char cor[tamanho_max];
-char tropashibri[tamanho_max];
-char tropasterrest[tamanho_max];
-char tropasaerea[tamanho_max];
+   char pais[tamanho_max];
+   char cor[tamanho_max];
+   int tropashibri;
+   int tropasterrest;
+   int tropasaerea;
 };
 
 void limparbuff(){
-int c;
-while((c = getchar()) != '\n' && c != EOF);
+   int c;
+   while((c = getchar()) != '\n' && c != EOF);
 };
 
 int main() {
-struct territ p1[max_livr];
-int opçao;
-int total = 0;
+   struct territ p1[max_livr];
+   int opçao;
+   int total = 0;
 
 do{
-printf("----------ESCOLHA UMA DAS OPÇOES A SEGUIR----------\n");
-printf("1 - CADASTRO DA CARTA\n");
+printf("\n----------ESCOLHA UMA DAS OPÇOES A SEGUIR----------\n");
+printf("1 - CADASTRO DA TERRITORIO\n");
 printf("2 - MOSTRA OS TERRITORIOS\n");
 printf("0 -  SAIR DO JOGO\n");
-printf("DIGITE A OPÇAO:");
+printf("DIGITE UMA OPÇAO: ");
 
 scanf("%d", &opçao);
 limparbuff();
 
 switch (opçao) {
     case 1:
-         printf("-----------CADASTRO DE CARTA----------\n\n");
+         printf("\n-----------CADASTRO DE CARTA----------\n\n");
            if (total < max_livr){
-           printf("DIGITE O NOME DO PAIS: ");
-           fgets(p1[total].pais, tamanho_max, stdin);
-           p1[total].pais[strcspn(p1[total].pais, "\n")] = 0;
+               printf("DIGITE O NOME DO PAIS: ");
+               fgets(p1[total].pais, tamanho_max, stdin);
+               p1[total].pais[strcspn(p1[total].pais, "\n")] = 0;
            
-           printf("DIGITE A COR: ");
-           fgets(p1[total].cor, tamanho_max, stdin);
-           p1[total].cor[strcspn(p1[total].cor, "\n")] = 0;
-}
-printf("\npressione enter");
-getchar();
-    break;
+               printf("DIGITE A COR: ");
+               fgets(p1[total].cor, tamanho_max, stdin);
+               p1[total].cor[strcspn(p1[total].cor, "\n")] = 0;
 
-default:
-    break;
-}
+               printf("DIGITE QUANTIDADE DE TROPAS HIBRIDAS: ");
+               scanf("%d", &p1[total].tropashibri);
+               limparbuff();
 
+               printf("DIGITE O TOTAL DE TROPAS AEREAS: ");
+               scanf("%d", &p1[total].tropasaerea);
+               limparbuff();
+
+               printf("DIGITE O TOTAL DE TROPAS TERRESTRES: ");
+               scanf("%d", &p1[total].tropasterrest);
+               limparbuff();
+
+        }     else{
+                 printf("TERRITORIOS DOMINADOS COMPLETAMENTE\n");
+        }
+          total++;
+
+          break;
+    case 2:
+          printf("INFORMAÇOES DE ESPIOES SOBRE TERRITORIOS\n\n");
+          if(total == 0) {
+             printf("NAO EXISTE TERRITORIO OCUPADO\n");
+        } 
+          else {
+              for(int i = 0; i < total; i++){
+                     printf("\nTERRITORIO ATIVO: %d\n", i +1);
+                     printf("PAIS: %s\n", p1[i].pais);
+                     printf("COR: %s\n", p1[i].cor);
+                     printf("TROPAS HIBRIDAS: %d\n", p1[i].tropashibri);
+                     printf("TROPAS AEREAS:%d\n", p1[i].tropasaerea);
+                     printf("TROPAS TERRESTRES: %d\n", p1[i].tropasterrest);
+        }
+                     printf("Pressione Enter.....");
+                     getchar();
+    }
+    case 0:
+         printf("SAINDO DO JOGO");
+         break; 
+
+    default:
+        printf("escolha invalida");
+        break;
+}
 
 }while (opçao != 0);
 return 0;
